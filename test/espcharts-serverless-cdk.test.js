da6d3a9677846357af27602f718d5ea753f38a47
Joan Shamet <shametijoan@gmail.com>
2023-09-12 11:49:30 +0200

    Initial commit

---
+++ test/espcharts-serverless-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const EspchartsServerlessCdk = require('../lib/espcharts-serverless-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/espcharts-serverless-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new EspchartsServerlessCdk.EspchartsServerlessCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
